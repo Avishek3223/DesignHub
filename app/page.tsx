07f888900aaafec2b0849fdbf8ee6d78a544178e
Avishek <avishekmishra56@gmail.com>
2024-08-22 08:16:59 +0530

    "Update Page component to wrap h1 in div with flex and centering styles"

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Page() {
   return (
-      <h1 className="font-bold text-[4rem]">Hello World</h1>
+    <div className="flex w-full h-screen justify-center items-center text-center">
+      <h1 className="font-bold text-white text-2xl">Hello World</h1>
+    </div>
   );
 }
