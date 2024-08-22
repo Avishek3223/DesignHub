15358e1882a32ab5b95fa51987720adf2087aafd
Avishek <avishekmishra56@gmail.com>
2024-08-22 16:40:32 +0530

    "Added Live component, updated Page component to use Live, and implemented LiveCursor with real-time cursor tracking"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+'use client'
+
+import Live from "@/components/Live";
 
 export default function Page() {
   return (
-    <div className="flex w-full h-screen justify-center items-center text-center">
-      <h1 className="font-bold text-white text-2xl">Hello World</h1>
-    </div>
+      <Live/>
   );
 }
--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import LiveCursor from './cursor/LiveCursor'
+import { useOthers } from '@liveblocks/react'
+import { useMyPresence } from '@/liveblocks.config';
 
 const Live = () => {
+    const others = useOthers(); //This hook return all the other user in the room
+    const [{ cursor }, updateMyPresence] = useMyPresence() as any; //Returns the presence of the current user of the current room
+
+    const handlePointerMove = useCallback((event: React.PointerEvent) => {
+        event.preventDefault()
+        const x = event.clientX - event.currentTarget.getBoundingClientRect().x
+        const y = event.clientY - event.currentTarget.getBoundingClientRect().y
+        updateMyPresence({
+            cursor: {
+                x: x,
+                y: y
+            },
+        });
+    }, [updateMyPresence]);
+
+    const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+        event.preventDefault()
+
+        updateMyPresence({ cursor: null, message: null });
+    }, []);
+
+
+    const handlePointerDown = useCallback((event: React.PointerEvent) => {
+        event.preventDefault()
+        updateMyPresence({
+            cursor: {
+                x: event.clientX - event.currentTarget.getBoundingClientRect().x,
+                y: event.clientY - event.currentTarget.getBoundingClientRect().y,
+            },
+        });
+    }, []);
+
     return (
-        <div>
-            <LiveCursor />
+        <div
+            onPointerMove={handlePointerMove}
+            onPointerLeave={handlePointerLeave}
+            onPointerDown={handlePointerDown}
+            className='flex w-full h-screen justify-center items-center text-center'
+        >
+            <h1 className="font-bold text-white text-2xl">Hello World</h1>
+            <LiveCursor others={others} />
         </div>
     )
 }
